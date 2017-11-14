6964ca56fab5afad197e7640cd71251355e9ffc2
niocy <juhaluoma5@gmail.com>
2017-11-14 09:21:07 +0200

    Initial push

---
+++ JunitTesting/src/main/java/com/mycompany/junittesting/JunitTesting.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.junittesting;
+
+import org.junit.Test;
+
+/**
+ *
+ * @author Suolapurkki
+ */
+public class JunitTesting {
+    
+    public int Age;
+    
+    @Test
+    public void DoCalculations(int test){
+        for(int i = 5; i > 0; i--){
+            int result = i / test;
+        }
+    }
+    
+    
+}
